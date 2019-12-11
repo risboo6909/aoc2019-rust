97fbb86a1af87f867e12651cde5aa056789dc61d
risboo6909 <ttyv00@gmail.com>
2019-12-12 00:47:06 +0300

    Add some vector math functions

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,10 +1,11 @@
 use std::fmt::{self, Display, Debug, Formatter};
-use core::ops::{Add, Sub};
+use core::ops::{Add, Sub, Mul, Div, AddAssign};
 
 use colored::*;
 use failure::Error;
 use num_traits::{Num, CheckedDiv, sign::Signed};
 use num::FromPrimitive;
+use failure::_core::iter::FromIterator;
 
 pub type ProblemResult<T> = Result<T, Error>;
 pub type ParseResult<T> = Result<T, Error>;
@@ -30,6 +31,44 @@ impl<T: Debug, K: Debug> Display for Ret<T, K> {
 
 pub fn man_dist_2d<T: Add<Output=T> + Sub<Output=T> + Signed>(x1: T, y1: T, x2: T, y2: T) -> T {
     num::abs(x1 - x2) + num::abs(y1 - y2)
+}
+
+pub fn dot_product<T: Add<Output=T> + Mul<Output=T> + AddAssign + Num + Copy>(xs: &[T], ys: &[T]) -> T {
+    let mut net: T = T::zero();
+    for (x, y) in xs.iter().zip(ys) {
+        net += *x * *y;
+    }
+    net
+}
+
+pub fn dot_product_2d<T: Add<Output=T> + Mul<Output=T> + AddAssign + Num + Copy>(x1: T, y1: T, x2: T, y2: T) -> T {
+    dot_product(&vec![x1, y1], &vec![x2, y2])
+}
+
+pub fn len<T: Add<Output=T> + Mul<Output=T> + AddAssign + Num + Copy>(coords: &[T]) -> f64 where f64: From<T> {
+    let mut net: T = T::zero();
+
+    for c in coords {
+        net += *c * *c;
+    }
+
+    f64::from(net).sqrt()
+}
+
+pub fn normalize<T: Div<Output=T> + AddAssign + Num + Copy>(coords: &[T]) -> Vec<f64> where f64: From<T> {
+    let l = len(coords);
+    let mut new_coords = Vec::new();
+
+    for c in coords {
+        new_coords.push(f64::from(*c) / l);
+    }
+
+    new_coords
+}
+
+pub fn normalize_2d<T: Div<Output=T> + AddAssign + Num + Copy>(coords: (T, T)) -> (f64, f64) where f64: From<T> {
+    let n = normalize(&vec![coords.0, coords.1]);
+    (n[0], n[1])
 }
 
 pub fn result<T: Debug, K: Debug>(basic: ProblemResult<T>, adv: ProblemResult<K>) -> Ret<T, K> {
